6f2835c3845a38d107873340a28baffbaf142cd2
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-12 23:00:55 +0000

    Fix typo in Navbar component CSS class
    A typo in the CSS class for the small icon section of the Navbar component was found and corrected. The incorrect class "bgr-textGeen" was changed to the correct class "bg-textGreen", ensuring the intended styling is applied.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -101,7 +101,7 @@ const Navbar = () => {
         </div>
         {/* SMALL ICON SECTION */}
         <div className="w-6 h-5 flex flex-col justify-between items-center md:hidden  text-textGreen cursor-pointer overflow-hidden group">
-          <span className="w-full h-[2px] bgr-textGeen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300"></span>
           <span className="w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300"></span>
         </div>
