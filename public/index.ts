374e7af9927c603e342cd5129b4a0049a31fac9b
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-12 21:34:15 +0000

    Add images and their exports to public index
    The public index file has been updated with the import of three new images. The images are a profile picture, a logo, and a logo with rounded corners. These imported images have been exported for use in other components.

---
+++ public/index.ts
@@ -0,0 +1,5 @@
+import profilePic from "./images/1633869148841.jpg";
+import logo from "./images/logo.png";
+import roundedLogo from "./images/logo_rounded.png";
+
+export { profilePic, logo, roundedLogo };
