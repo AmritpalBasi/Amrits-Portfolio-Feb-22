0d9cba41a8dc4a160104e2b49e65f680202728cf
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-12 22:28:31 +0000

    Add new Navbar component
    A new React component, Navbar, has been added. This component features a sticky navbar with animated links for Home, About, Experience, Projects, and Contact sections, and also includes a Resume button. The implementation uses Next.js Link for navigation and Framer Motion for animations.

---
+++ components/Navbar.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from "react";
+import { logo } from "@/public";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+      <div className="max-w-container h-full mx-auto py-1 font-inter flex items-center justify-between">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image src={logo} alt="logo" className="w-14" />
+        </motion.div>
+        <div>
+          <ul className="flex text-[13px] gap-7">
+            {/*LINK 1: HOME*/}
+            <Link
+              href="#home"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+              >
+                Home
+              </motion.li>
+            </Link>
+            {/*LINK 2: ABOUT*/}
+            <Link
+              href="#about"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.2 }}
+              >
+                <span className="text-textGreen">01.</span> About
+              </motion.li>
+            </Link>
+
+            {/*LINK 3: EXPERIENCE*/}
+            <Link
+              href="#experience"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.3 }}
+              >
+                <span className="text-textGreen">02.</span> Experience
+              </motion.li>
+            </Link>
+
+            {/*LINK 4: PROJECTS*/}
+            <Link
+              href="#projects"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.4 }}
+              >
+                <span className="text-textGreen">03.</span> Projects
+              </motion.li>
+            </Link>
+
+            {/*LINK 5: CONTACT*/}
+            <Link
+              href="#contact"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1, delay: 0.5 }}
+              >
+                <span className="text-textGreen">04.</span> Contact
+              </motion.li>
+            </Link>
+            {/*IMPLEMENT RESUME BUTTON*/}
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5 }}
+              className="px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
+            >
+              Resume
+            </motion.button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
