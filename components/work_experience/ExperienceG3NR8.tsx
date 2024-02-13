39be5079687a131dc6d03a3114b9471f7d1dd2ce
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 22:43:52 +0000

    Update ExperienceG3NR8 component
    The ExperienceG3NR8 component has been updated to include new details using motion transitions. A more detailed breakdown of the user's work experience is now presented. The typography has been optimized to improve legibility, presenting the user's work experience in a clearer and more engaging way.

--- components/work_experience/ExperienceG3NR8.tsx
+++ components/work_experience/ExperienceG3NR8.tsx
@@ -1,5 +1,58 @@
-const ExperienceG3Nr8 = () => {
-  return <div>G3NR8</div>;
+"use client";
+import { motion } from "framer-motion";
+import { TiArrowForward } from "react-icons/ti";
+
+const ExperienceG3NR8 = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.1 }}
+    >
+      <h3 className="flex gap-1 font-medium text-xl font-inter">
+        Machine Learning Engineer{" "}
+        <span className="text-textGreen tracking-wide">@G3NR8</span>
+      </h3>
+
+      <p className="text-sm mt-1 font-medium text-textDark">
+        Nov 2023 - Present
+      </p>
+      {/* LIST EXPERIENCE */}
+      <ul className="mt-6 flex flex-col gap-3">
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+      </ul>
+    </motion.div>
+  );
 };
 
-export default ExperienceG3Nr8;
+export default ExperienceG3NR8;
