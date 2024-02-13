f9f11a33688f6682b8998cdfb9d8ee52b1742f2d
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 12:52:57 +0000

    Update layout styles in page.tsx
    Revised the layout styles in page.tsx to improve UI presentation. By adding new style classes to the left-side and right-side div elements and adding test divs to the middle div, the changes enhance the overall page structure and visual differentiation.

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,17 @@ export default function Home() {
       <Navbar />
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         {/* left hand side */}
-        <div>
+        <div className="hidden md:inline-flex w-32 h-full fixed left-0 bottom-0">
           <LeftSide />
         </div>
         {/* middle of screen */}
-        <div></div>
+        <div>
+          <div className="bg-red-300">test</div>
+        </div>
         {/* right hand side of screen */}
-        <div></div>
+        <div>
+          <div className="bg-amber-300">test</div>
+        </div>
       </div>
     </main>
   );
