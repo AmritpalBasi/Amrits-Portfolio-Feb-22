3136d2b30eb1a5c774ec1134375338c291c07157
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 22:18:33 +0000

    Update Experience component structure
    Refactored the Experience component to render a section with the title "Where I've Worked" and a list of previous workplaces. The component now provides a stylized and interactive view for users.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,5 +1,30 @@
+import SectionTitle from "@/components/SectionTitle";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section
+      id="experience"
+      className="max-w-containerXs mx-auto py-10 lgl:py-24 px-4"
+    >
+      <SectionTitle title="Where I've Worked" titleNo="02" />
+      <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
+        <ul className="md:w-32 flex flex-col">
+          <li className="border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            G3NR8
+          </li>
+          <li className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            Milvik Bima
+          </li>
+          <li className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            ETX Capital
+          </li>
+          <li className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium">
+            Leesman
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
