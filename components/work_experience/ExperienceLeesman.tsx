37dac9caa8503a7f1beb47afbf1189343ff2d804
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 22:54:12 +0000

    Add ExperienceLeesman.tsx component
    A new component, ExperienceLeesman, has been introduced. This component provides details of my work experience

---
+++ components/work_experience/ExperienceLeesman.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { motion } from "framer-motion";
+import { TiArrowForward } from "react-icons/ti";
+
+const ExperienceG3NR8 = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.1 }}
+    >
+      <h3 className="flex gap-1 font-medium text-xl font-inter">
+        Data Analyst{" "}
+        <span className="text-textGreen tracking-wide">@G3NR8</span>
+      </h3>
+
+      <p className="text-sm mt-1 font-medium text-textDark">
+        Sep 2019 - Oct 2021
+      </p>
+      {/* LIST EXPERIENCE */}
+      <ul className="mt-6 flex flex-col gap-3">
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+
+        <li className="text-base flex gap-2 text-textDark">
+          <span className="text-textGreen mt-1">
+            <TiArrowForward />
+          </span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </li>
+      </ul>
+    </motion.div>
+  );
+};
+
+export default ExperienceG3NR8;
