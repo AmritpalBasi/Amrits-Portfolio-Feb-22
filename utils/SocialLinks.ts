576a905d5cb6666ff1e9d7a59b6581ba6b1177ca
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 12:37:40 +0000

    Add fields to SocialLinksType and socialLinks
    Additional fields including Email, Phone, and Instagram has been added to the SocialLinksType definition. Correspondingly, the actual values for these fields have been set in the socialLinks constant.

--- utils/SocialLinks.ts
+++ utils/SocialLinks.ts
@@ -1,11 +1,17 @@
 export type SocialLinksType = {
   Github: string;
   LinkedIn: string;
+  Email: string;
+  Phone: string;
   Twitter: string;
+  Instagram: string;
 };
 
 export const socialLinks: SocialLinksType = {
   Github: "https://github.com/AmritpalBasi",
   LinkedIn: "https://www.linkedin.com/in/amritpal-basi/",
+  Email: "amritpalbasi@gmail.com",
+  Phone: "+44 7376366990",
   Twitter: "https://twitter.com/Amrit_XL",
+  Instagram: "https://www.instagram.com/amrit_basi/",
 };
