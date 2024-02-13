00ca3a465501ba53266faa27db29d4fe9244e155
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 13:35:02 +0000

    Add Middle and RightSide components to Home page
    These components were added along with necessary CSS styling and responsive classes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
 import Navbar from "@/components/Navbar";
 import LeftSide from "@/components/LeftSide";
+import RightSide from "@/components/RightSide";
+import Middle from "@/components/Middle";
 
 export default function Home() {
   return (
     <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
-      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+      <div className="w-full h-[88vh] mdl:flex items-center gap-20 justify-between">
         {/* left hand side */}
-        <div className="hidden md:inline-flex w-32 h-full fixed left-0 bottom-0">
+        <div className="hidden mdl:inline-flex w-32 h-full fixed left-0 bottom-0">
           <LeftSide />
         </div>
         {/* middle of screen */}
-        <div>
-          <div className="bg-red-300">test</div>
+        <div className="h-[88vh] mx-auto p-4">
+          <Middle />
         </div>
         {/* right hand side of screen */}
-        <div>
-          <div className="bg-amber-300">test</div>
+        <div className="hidden mdl:inline-flex w-32 h-full fixed right-0 bottom-0">
+          <RightSide />
         </div>
       </div>
     </main>
