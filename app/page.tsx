6e4b67cafb6d1832a3decc7af68c446379c7b53c
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 13:40:57 +0000

    Add animations to LeftSide and RightSide components
    This commit introduces "framer-motion" library and uses it to add loading animations to the LeftSide and RightSide components. These components now appear gradually with a delay that improves the transition and overall user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
 import Middle from "@/components/Middle";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -9,17 +11,27 @@ export default function Home() {
       <Navbar />
       <div className="w-full h-[88vh] mdl:flex items-center gap-20 justify-between">
         {/* left hand side */}
-        <div className="hidden mdl:inline-flex w-32 h-full fixed left-0 bottom-0">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className="hidden mdl:inline-flex w-32 h-full fixed left-0 bottom-0"
+        >
           <LeftSide />
-        </div>
+        </motion.div>
         {/* middle of screen */}
         <div className="h-[88vh] mx-auto p-4">
           <Middle />
         </div>
         {/* right hand side of screen */}
-        <div className="hidden mdl:inline-flex w-32 h-full fixed right-0 bottom-0">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className="hidden mdl:inline-flex w-32 h-full fixed right-0 bottom-0"
+        >
           <RightSide />
-        </div>
+        </motion.div>
       </div>
     </main>
   );
