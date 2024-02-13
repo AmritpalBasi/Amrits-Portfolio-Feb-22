665953a9528c08e2a15528c1602742d1cd6bc05f
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 15:43:25 +0000

    Add Banner component
    A new Banner component has been added to the project. This component includes a home section filled with motion elements like headers and paragraphs, and also incorporates an external link to learn more.

---
+++ components/Banner.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { motion } from "framer-motion";
+import { socialLinks } from "@/utils/SocialLinks";
+
+const Banner = () => {
+  return (
+    <section
+      id="home"
+      className="max-w-contentContainer mx-auto py:10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
+    >
+      <motion.h3
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.6 }}
+        className="text-lg font-inter text-textGreen tracking-wide"
+      >
+        Hey, my name is
+      </motion.h3>
+      <motion.h1
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        className="text-4xl lgl:text-6xl font-inter font-semibold flex flex-col"
+      >
+        Amrit Basi
+        <span className="text-textDark mt-2 lgl:mt-4">
+          Building for Analytics and Machine Learning
+        </span>
+      </motion.h1>
+      <motion.p
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.8 }}
+        className="text-base md:max-w-[650px] text-textDark font-medium"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo.{" "}
+        <a href={socialLinks.LinkedIn} target="_blank">
+          {" "}
+          <span className="text-textGreen inline-flex relative cursor-pointer h-7 overflow-x-hidden group">
+            Learn More
+            <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
+          </span>
+        </a>
+      </motion.p>
+    </section>
+  );
+};
+
+export default Banner;
