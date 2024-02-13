183f62a2e0f1a682abed0659d82ba84e94f806ec
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 11:59:49 +0000

    Add SocialLinks utility file
    A new utility file named SocialLinks.ts has been introduced. This file contains the type definition for social links and exports actual social links of specific social platforms such as Github, LinkedIn, and Twitter.

---
+++ utils/SocialLinks.ts
@@ -0,0 +1,11 @@
+export type SocialLinksType = {
+  Github: string;
+  LinkedIn: string;
+  Twitter: string;
+};
+
+export const socialLinks: SocialLinksType = {
+  Github: "https://github.com/AmritpalBasi",
+  LinkedIn: "https://www.linkedin.com/in/amritpal-basi/",
+  Twitter: "https://twitter.com/Amrit_XL",
+};
