1d6d099f47953a0e419c1beaa2b3ede14631cd9c
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 11:31:35 +0000

    Add LeftSide component to Home page
    The LeftSide component is now included in the Home function of the page.tsx file. This is represented in a div, and placed on the left hand side of the screen layout. The remaining middle and right-hand portion of the screen have also been prepared for future components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Navbar from "@/components/Navbar";
+import LeftSide from "@/components/LeftSide";
 
 export default function Home() {
   return (
     <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        {/* left hand side */}
+        <div>
+          <LeftSide />
+        </div>
+        {/* middle of screen */}
+        <div></div>
+        {/* right hand side of screen */}
+        <div></div>
+      </div>
     </main>
   );
 }
