e2a87339f7d7e3fd78995547f99130608fc48235
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-13 15:53:56 +0000

    Replace Middle component with Banner component in Home
    The main component located in the center of the Home page has been changed from Middle to Banner. Furthermore, placeholders for additional sections such as About, Experience, Projects, Archive, Contact, and Footer have been added. The changes are aimed at improving user experience and navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,39 @@
 import Navbar from "@/components/Navbar";
 import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
-import Middle from "@/components/Middle";
+import Banner from "@/components/Banner";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
       <Navbar />
-      <div className="w-full h-[88vh] mdl:flex items-center gap-20 justify-between">
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         {/* left hand side */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }}
-          className="hidden mdl:inline-flex w-32 h-full fixed left-0 bottom-0"
+          className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0"
         >
           <LeftSide />
         </motion.div>
         {/* middle of screen */}
-        <div className="h-[88vh] mx-auto p-4">
-          <Middle />
+        <div className="h-[88vh] w-full mx-auto p-4">
+          <Banner />
+          {/* ABOUT */}
+          {/* EXPERIENCE */}
+          {/* PROJECTS */}
+          {/* ARCHIVE */}
+          {/*  CONTACT */}
+          {/*  FOOTER */}
         </div>
         {/* right hand side of screen */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }}
-          className="hidden mdl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
         >
           <RightSide />
         </motion.div>
