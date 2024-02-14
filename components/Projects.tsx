ba5f8cba46dc71d2510a25a6996725d4cc033b5a
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 14:31:58 +0000

    Add new Projects component
    Introduced a new Projects component within the components directory. This component, at present, simply returns a div containing a SectionTitle component with the title set to "Projects".

---
+++ components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "@/components/SectionTitle";
+
+const Projects = () => {
+  return (
+    <div>
+      <SectionTitle title="Projects" titleNo="03" />
+    </div>
+  );
+};
+
+export default Projects;
