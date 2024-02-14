8de361f9544460f2dcdf2a949502a5040ca14cfd
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 15:05:29 +0000

    Update index.ts to include an import and export of for the Skiing image

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,6 @@
 import profilePic from "./images/1633869148841.jpg";
 import logo from "./images/logo.png";
 import roundedLogo from "./images/logo_rounded.png";
+import SkiGPT from "./images/skiing-image.webp";
 
-export { profilePic, logo, roundedLogo };
+export { profilePic, logo, roundedLogo, SkiGPT };
