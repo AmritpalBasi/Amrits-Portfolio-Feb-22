2fccc0322db311fccd906c00f002cdb853c86be8
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 21:38:25 +0000

    This section includes a title, introduction, and a 'Say Hello!' button linking to the LinkedIn profile.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,29 @@
+import { portfolioText } from "@/utils/PortfolioText";
+import { socialLinks } from "@/utils/SocialLinks";
+
 const Contact = () => {
-  return <div>Contact Me</div>;
+  return (
+    <section
+      id="contact"
+      className="max-w-contentContainer mx-auto py-10 xl:py:32 flex flex-col gap-4 items-center justify-center"
+    >
+      <p className="font-inter text-lg text-textGreen font-semibold flex items-center tracking-wide">
+        04. What's Next
+      </p>
+      <h2 className="font-inter text-5xl font-semibold">Get in Touch</h2>
+      <p className="max-w-[600px] text-center text-textDark">
+        {portfolioText.contactMe.text}
+      </p>
+      <a href={socialLinks.LinkedIn} target="_blank">
+        <button
+          className="w-40 h-14 border border-textGreen rounded-md text-sm text-textGreen
+          font-inter tracking-wider hover:bg-hoverColor duration-300 mt-6"
+        >
+          Say Hello!
+        </button>
+      </a>
+    </section>
+  );
 };
 
 export default Contact;
