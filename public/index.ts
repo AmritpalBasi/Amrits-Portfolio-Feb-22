fb952842048f5a245f1c6229683a8d0bbc26e634
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 17:29:25 +0000

    Add diamondGoose image import and export
    The image 'diamond-goose-ad-suite' has been imported to the index.ts file in the public directory. It has also been added to the list of exported assets.

--- public/index.ts
+++ public/index.ts
@@ -2,5 +2,6 @@ import profilePic from "./images/1633869148841.jpg";
 import logo from "./images/logo.png";
 import roundedLogo from "./images/logo_rounded.png";
 import SkiGPT from "./images/skiing-image.webp";
+import diamondGoose from "./images/diamond-goose-ad-suite.webp";
 
-export { profilePic, logo, roundedLogo, SkiGPT };
+export { profilePic, logo, roundedLogo, SkiGPT, diamondGoose };
