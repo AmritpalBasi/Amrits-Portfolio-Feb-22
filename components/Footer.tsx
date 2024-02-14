83d65a1481584985eb553817d2092831accfd45c
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 21:54:05 +0000

    Add Footer component with social links
    A new Footer component is created which includes various social media icons. Each icon is linked to the relevant social media platforms, including GitHub, LinkedIn, Email, Phone, Twitter, and Instagram.

---
+++ components/Footer.tsx
@@ -0,0 +1,44 @@
+import { TbBrandGithub } from "react-icons/tb";
+import { SlSocialLinkedin, SlSocialInstagram } from "react-icons/sl";
+import { BsTwitterX } from "react-icons/bs";
+import { MdOutlineEmail, MdSmartphone } from "react-icons/md";
+import { socialLinks } from "@/utils/SocialLinks";
+
+const Footer = () => {
+  return (
+    <div className="hidden md:inline-flex xl:hidden items-center justify-center w-full py-6 gap-4">
+      <a href={socialLinks.Github} target="_blank">
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <TbBrandGithub />
+        </span>
+      </a>
+      <a href={socialLinks.LinkedIn} target="_blank">
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialLinkedin />
+        </span>
+      </a>
+      <a href={`mailto:${socialLinks.Email}`}>
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <MdOutlineEmail />
+        </span>
+      </a>
+      <a href={`tel:${socialLinks.Phone}`}>
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <MdSmartphone />
+        </span>
+      </a>
+      <a href={socialLinks.Twitter} target="_blank">
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <BsTwitterX />
+        </span>
+      </a>
+      <a href={socialLinks.Instagram} target="_blank">
+        <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+          <SlSocialInstagram />
+        </span>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
