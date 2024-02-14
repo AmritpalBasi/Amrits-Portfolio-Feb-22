cb081a66c9f4227c94f242576de52baa44d62b2c
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-14 17:28:55 +0000

    Create utils/PortfolioText.tsx to hold portfolio descriptions
    Introduced a new file, utils/PortfolioText.tsx, to contain the descriptive text for portfolio projects. Currently, this encompasses two projects - SKI-GPT and Diamond Goose Ad-Suite - each furnished with a title and a descriptive text passage.

---
+++ utils/PortfolioText.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const portfolioText = {
+  projectSkiGPT: {
+    title: "SKI-GPT",
+    text: (
+      <>
+        {" "}
+        {/* Node wrapper */}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam,{" "}
+        <span className="text-textGreen">
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </span>
+      </>
+    ),
+  },
+
+  projectDiamondGoose: {
+    title: "Diamond Goose Ad-Suite",
+    text: (
+      <>
+        {" "}
+        {/* Node wrapper */}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam,{" "}
+        <span className="text-textGreen">
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </span>
+      </>
+    ),
+  },
+};
