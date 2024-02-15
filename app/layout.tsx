106bddafbb76be07312161581b24256393d44e91
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-15 17:37:49 +0000

    Integrate Vercel's Analytics in app layout
    Vercel's Analytics package "@vercel/analytics" has been added as a dependency and is now imported in the app layout. Consequently, an "<Analytics />" component is included in the body of the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${montserrat.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
