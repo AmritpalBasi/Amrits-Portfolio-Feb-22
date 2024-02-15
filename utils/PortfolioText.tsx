8e3c0b64cdcd0e0fa44359ab0153b7af53c14a0e
Amritpal Basi <amritpalbasi@gmail.com>
2024-02-15 14:29:00 +0000

    Add BannerText to PortfolioText component
    Introduced and integrated the new 'bannerText' property into the portfolioText component.

--- utils/PortfolioText.tsx
+++ utils/PortfolioText.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 
 export const portfolioText = {
+  bannerText: {
+    text: (
+      <>
+        <>
+          It's one thing to build a great model, it's another to get people to
+          care and trust it. That's been my main mission working in different
+          stage start-ups, some of those with funding milestones ranging from{" "}
+          <span className="text-textGreen">$5M to $200M.</span> My models have
+          been responsible for finding{" "}
+          <span className="text-textGreen">
+            hundreds of thousands in unreconciled revenue, detecting internal
+            fraud and responsible for allocating marketing resources.
+          </span>
+          {"\n"}
+        </>
+        <div className="mt-3">
+          That code running in a cloud server somewhere will have financial and
+          ethical implications, so ensuring everybody is on the same page is
+          important.
+        </div>
+      </>
+    ),
+  },
+
   projectSkiGPT: {
     title: "SKI-GPT",
     text: (
       <>
         {" "}
-        {/* Node wrapper */}
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam,{" "}
